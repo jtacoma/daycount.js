5daec4d966fde2f1d5c5accb78596e007338df72
Joshua Tacoma <joshua@yellowseed.org>
2011-02-18 20:47:49 +0000

    updated gh-pages for v0.0.2.

--- js/daycount.js
+++ js/daycount.js
@@ -1,56 +1,103 @@
-/**
- * Top level namespace for daycount.js, a general purpose calendar
- * and date calculations library.
+/*
+ * daycount.js v0.0.2
+ * http://yellowseed.org/daycount.js/
+ *
+ * Copyright 2011, Joshua Tacoma
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ *
  */
+
+// All other globals defined in this module are stored within this object:
 var daycount = {};
 
-/**
- * The Day type, which may include associated information from any calendar system.
- */
-daycount.day = (function() {
-  function day(arg) {
+// The 'moment' type, which may include associated information from any calendar system:
+daycount.moment = (function() {
+
+  // 'moment' constructor:
+  function moment(arg) {
+    this.set(arg);
+  };
+
+  moment.prototype.set = function(arg) {
     if(!arg || arg === null)
       arg = new Date();
-    var builder_name = 'from_' + arg.constructor.name;
+
+    // Store argument as the only known property of this:
     this[arg.constructor.name] = arg;
+
+    // Now, we're going to calculate as many counts as possible...
+
+    // 'done' lists known counts:
     var done = [arg.constructor.name];
+
+    // 'todo' lists counts to be calculated:
     var todo = []
     for(var name in daycount.counts)
       if(!this.hasOwnProperty(name))
         todo.push(name);
+
     var finished = false;
     while(!finished) {
+
+      // Assume that there is nothing left to do, then try to prove
+      // that assumption incorrect:
       finished = true;
-      for (var indexTodo = todo.length-1; indexTodo >= 0; --indexTodo) {
+
+      // Iterate through counts in 'todo'.  Since we're going to remove
+      // them as we go, iterate backwards to keep remaining indices
+      // from shifting:
+      for (var indexTodo = todo.length - 1; indexTodo >= 0; --indexTodo) {
         var nameTodo = todo[indexTodo];
         var countTodo = daycount.counts[nameTodo];
+
+        // Iterate through known counts in 'done', looking for something
+        // we can calculate 'countTodo' from:
         for (var indexDone = 0; indexDone < done.length; ++indexDone) {
           var nameDone = done[indexDone];
           var builderNameTodo = 'from_' + nameDone;
           if(!countTodo.hasOwnProperty(builderNameTodo)) continue;
+
+          // Found one!  Calculate the value for 'countTodo' and remove
+          // it from 'done':
           var builder = countTodo[builderNameTodo];
           this[nameTodo] = builder(this[nameDone]);
           done.push(nameTodo);
           todo.splice(indexTodo, 1)
+
+          // Since we've just calculated another count, any remaining
+          // counts must be recalculated, meaning we're not finished:
           finished = false;
           break;
         }
       }
     }
   };
-  return day;
+
+  moment.prototype.incrementEarthSolarDays = function(days) {
+    if (this['gregorian'])
+      this.set(this.gregorian.plusDays(days));
+    else if(this['julianDay'])
+      this.set(this.julianDay.plusDays(days));
+    else
+      throw 'this moment has no counts that support the specified increment.';
+  };
+
+  moment.prototype.increment = moment.prototype.incrementEarthSolarDays;
+
+  return moment;
 })();
 
-/**
- * A collection of counts i.e. calendar systems.  Each calendar system should be added to this object.
- */
+daycount.day = daycount.moment;
+
+// A collection of counts i.e. calendar systems.
+// Each calendar system should be added to this object.
 daycount.counts = {};
 
 daycount.version_ = {
   major: 0,
   minor: 0,
-  build: 1,
-  revision: 1,
+  build: 2,
 };
 
 daycount.counts.dreamspell = (function () {
@@ -196,10 +243,10 @@ daycount.counts.julianDay = (function() {
 
   julianDay.from_Date = function(system) {
     // from Wikipedia's Julian_day article:
-    var a = parseInt((13 - system.getMonth()) / 12);
-    var y = system.getFullYear() + 4800 - a;
-    var m = system.getMonth() + (12 * a) - 2;
-    var number = system.getDate() + Math.floor((153 * m + 2) / 5)
+    var a = parseInt((13 - system.getUTCMonth()) / 12);
+    var y = system.getUTCFullYear() + 4800 - a;
+    var m = system.getUTCMonth() + (12 * a) - 2;
+    var number = system.getUTCDate() + Math.floor((153 * m + 2) / 5)
              + 365 * y + Math.floor(y / 4) - Math.floor(y / 100)
              + Math.floor(y / 400) - 32045;
     return new daycount.counts.julianDay({
